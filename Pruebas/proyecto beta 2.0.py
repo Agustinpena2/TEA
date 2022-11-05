1326161d0ea6e30015c8567e6332d48f9b5993d8
Esteban Agustin Penados Betancourt (24045) <esteban.penados@est.zamorano.edu>
2022-11-04 20:39:35 -0600

    Base de DatosT

---
+++ Pruebas/proyecto beta 2.0.py
@@ -0,0 +1,21 @@
+import datetime
+fechas = ['2022-11-03', '2022-11-03', '2022-11-03', '2022-11-03']
+fecha = datetime.date.today()
+year = fecha.year
+for y in precipitacion:
+    if (y.anno == 2016): 
+    elif (y.anno == 2017): 
+    elif (y.anno == 2018):
+    elif (y.anno == 2019)
+    elif (y.anno == 2020)
+    elif (y.anno == 2021)
+    elif (y.anno == 2022)
+    .
+    else: sdhshdkjshf
+
+for x in range(2016, year):
+    print(x)
+
+    #print(fechas)
+#print(fecha)
+#print(year)
